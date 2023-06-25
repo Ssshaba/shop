98772ed8b394a4a5f29ac0d89be6f6182356b00e
Ssshaba <maks2015611@gmail.com>
2023-06-26 01:26:15 +0300

    Back and Front Connect

--- app/Http/Controllers/Product/StoreController.php
+++ app/Http/Controllers/Product/StoreController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Product;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Product\StoreRequest;
 use App\Models\Color;
+use App\Models\Tag;
 use App\Models\ColorProduct;
 use App\Models\Product;
 use App\Models\ProductTag;
